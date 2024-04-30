4e22755b6aab8c8acf6890a7d0fb2f987133615f
Ruslan <Giovingo92@gmail.com>
2024-04-30 19:32:42 +0300

    fix: content-type

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
+app.use(express.json());
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
