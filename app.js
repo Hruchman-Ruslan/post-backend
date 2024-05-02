0b19d0361f2aed5bfafef4b6da21c2138bc2760c
Ruslan <Giovingo92@gmail.com>
2024-05-02 23:14:26 +0300

    feat: get post posts

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-import cors from "cors";
-import logger from "morgan";
 import express from "express";
+import logger from "morgan";
+import cors from "cors";
+
+import postsRouter from "./routes/posts.js";
 
 const app = express();
 
@@ -9,6 +11,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/posts", postsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 import express from "express";
 
-import { addPost, getAllPosts } from "../controllers";
+import { addPost, getAllPosts } from "../controllers/index.js";
 
 const router = express.Router();
 
 router.get("/", getAllPosts);
 
 router.post("/", addPost);
+
+export default router;
